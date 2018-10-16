e1e666515bd95df7ff337f80f2be45a8d9aeee4f
Slavomír Bavala <slavobavala@gmail.com>
2018-10-16 21:05:45 +0200

    Initial commit

---
+++ src/main/java/sk/xidex/loansrestapi/LoansRestApiApplication.java
@@ -0,0 +1,12 @@
+package sk.xidex.loansrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansRestApiApplication.class, args);
+	}
+}
