7feb459af7aa68ce9096867c04a2d8d818e6905e
Slavomir Bavala <slavomir.bavala@performgroup.com>
2018-10-17 08:38:03 +0200

    fixed implementation of average loan amount calculator

--- src/main/java/sk/xidex/loansrestapi/service/ZonkyApiServiceImpl.java
+++ src/main/java/sk/xidex/loansrestapi/service/ZonkyApiServiceImpl.java
@@ -10,43 +10,51 @@ import sk.xidex.loansrestapi.service.model.Loan;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ZonkyApiServiceImpl implements ZonkyApiService {
 
-	private static final String LIST_LOANS = "https://private-anon-09c3ac41f9-zonky.apiary-proxy.com/loans/marketplace";
+    private static final String LIST_LOANS = "https://private-anon-09c3ac41f9-zonky.apiary-proxy.com/loans/marketplace";
 
-	private RestTemplate restTemplate;
+    protected int pageSize = 1000;
+    private RestTemplate restTemplate;
 
-	@Autowired
-	public ZonkyApiServiceImpl(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
+    @Autowired
+    public ZonkyApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-	@Override
-	public double calculateAverageLoanAmount(String rating) {
-		String url = LIST_LOANS + "?rating__eq=" + rating + "&fields=amount";
-		List<Double> averages = new ArrayList<>();
-		ResponseEntity<List<Loan>> response;
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-		Integer pageNr = 0;
-		headers.add("x-page", pageNr.toString());
-		headers.add("x-size", "1000");
-		do {
-			HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-			response = restTemplate.exchange(
-					url,
-					HttpMethod.GET,
-					entity,
-					new ParameterizedTypeReference<List<Loan>>() {
-					}
-			);
-			response.getBody().parallelStream().mapToDouble(Loan::getAmount).average().ifPresent(averages::add);
-			pageNr++;
-			headers.set("x-page", pageNr.toString());
-		} while (!response.getBody().isEmpty());
+    @Override
+    public double calculateAverageLoanAmount(String rating) {
+        String url = LIST_LOANS + "?rating__eq=" + rating + "&fields=amount";
+        List<Double> averages = new ArrayList<>();
+        ResponseEntity<List<Loan>> response;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        int pageNr = 0;
+        headers.add("x-page", Integer.toString(pageNr));
+        headers.add("x-size", Integer.toString(pageSize));
+        do {
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+            response = restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    entity,
+                    new ParameterizedTypeReference<List<Loan>>() {
+                    }
+            );
+            if (response.getBody() == null) {
+                break;
+            }
+            averages.addAll(response.getBody().parallelStream().map(Loan::getAmount).collect(Collectors.toList()));
+            if (response.getBody().size() < pageSize) {
+                break;
+            }
+            pageNr++;
+            headers.set("x-page", Integer.toString(pageNr));
+        } while (!response.getBody().isEmpty());
 
-		return averages.parallelStream().mapToDouble(Double::doubleValue).average().orElse(0);
-	}
+        return averages.parallelStream().mapToDouble(Double::doubleValue).average().orElse(0);
+    }
 }
--- src/test/java/sk/xidex/loansrestapi/service/ZonkyApiServiceTest.java
+++ src/test/java/sk/xidex/loansrestapi/service/ZonkyApiServiceTest.java
@@ -1,53 +1,54 @@
 package sk.xidex.loansrestapi.service;
 
 import org.junit.Assert;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 import sk.xidex.loansrestapi.service.model.Loan;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class ZonkyApiServiceTest {
 
-	@Mock
-	private RestTemplate restTemplate;
+    @Mock
+    private RestTemplate restTemplate;
+    @InjectMocks
+    private ZonkyApiServiceImpl zonkyApiService;
 
-	@Autowired
-	private ZonkyApiService zonkyApiService;
+    @Test
+    public void calculateAverageLoanAmount_incorrectRatingGiven_zeroAverageReturned() {
+        Mockito.when(restTemplate.exchange(Mockito.anyString(), Mockito.any(HttpMethod.class), Mockito.any(), ArgumentMatchers.<ParameterizedTypeReference<List<Loan>>>any()))
+                .thenReturn(createResponse())
+                .thenReturn(createSecondResponse());
+        zonkyApiService.pageSize = 3;
 
-	@Ignore
-	public void calculateAverageLoanAmount_incorrectRatingGiven_zeroAverageReturned() {
-		Mockito.when(restTemplate.exchange(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(ParameterizedTypeReference.class)))
-				.thenReturn(createResponse())
-				.thenReturn(createSecondResponse());
+        double average = zonkyApiService.calculateAverageLoanAmount("D");
+        Assert.assertEquals(1750, average, 0.0);
+        zonkyApiService.pageSize = 1000;
+    }
 
-		double average = zonkyApiService.calculateAverageLoanAmount("D");
-		Assert.assertEquals(1750, average, 0.0);
-	}
+    private ResponseEntity<List<Loan>> createResponse() {
+        List<Loan> loans = new ArrayList<>();
+        loans.add(new Loan(1000));
+        loans.add(new Loan(2000));
+        loans.add(new Loan(3000));
+        return new ResponseEntity<>(loans, HttpStatus.OK);
+    }
 
-	private ResponseEntity<List<Loan>> createResponse() {
-		List<Loan> loans = new ArrayList<>();
-		loans.add(new Loan(1000));
-		loans.add(new Loan(2000));
-		loans.add(new Loan(3000));
-		return new ResponseEntity<>(loans, HttpStatus.OK);
-	}
-
-	private ResponseEntity<List<Loan>> createSecondResponse() {
-		List<Loan> loans = new ArrayList<>();
-		loans.add(new Loan(1000));
-		return new ResponseEntity<>(loans, HttpStatus.OK);
-	}
+    private ResponseEntity<List<Loan>> createSecondResponse() {
+        List<Loan> loans = new ArrayList<>();
+        loans.add(new Loan(1000));
+        return new ResponseEntity<>(loans, HttpStatus.OK);
+    }
 }
